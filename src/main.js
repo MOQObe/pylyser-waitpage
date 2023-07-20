8a78c4907c020fc54301af440e9ee7b5a2b55ee3
Arno Ramon <arno@moqo.be>
2023-07-20 11:43:00 +0200

    Fix safari height

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", () => {
    document.querySelector('body').classList.add('Loaded');
+   document.querySelector('body').style.maxHeight = window.innerHeight + "px";
+   
+   window.addEventListener("resize", (e) => {
+      document.querySelector('body').style.maxHeight = window.innerHeight + "px";
+   });
+
 });
